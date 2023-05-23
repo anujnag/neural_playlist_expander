53dcbac32721b76adfc22a82d26c77752f484960
Anuj Nagpal <anujnagpal96@gmail.com>
2023-05-22 23:48:43 -0700

    Mask candidate generation code

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import random
+from web_api import generate_candidate_list
 
 def get_positive_track(playlist):
     idx = random.randint(0, len(playlist))
@@ -12,4 +13,8 @@ def get_negative_track(playlist, all_tracks):
         idx = random.randint(0, len(all_tracks))    
         track = all_tracks[idx]
 
-    return track    
+    return track
+
+def generate_candidate_list(playlist):
+    # code added in a private file to hide API keys and access tokens
+    return generate_candidate_list(playlist)       
