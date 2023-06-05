1aa0cd0e96c21abaae8b5cc6c80605637e25360e
Anuj Nagpal <anujnagpal96@gmail.com>
2023-06-04 18:38:24 -0700

    Improve artist feature builder

--- feature_builder.py
+++ feature_builder.py
@@ -12,6 +12,8 @@ def write_data_to_csv(data_dicts, type):
     
     if type == 'artist':
         fieldnames = artist_fieldnames
+    elif type == 'track':
+        fieldnames = track_fieldnames    
     
     with open(csv_filename, 'w', newline='', encoding='UTF8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -21,17 +23,33 @@ def write_data_to_csv(data_dicts, type):
         for dict in data_dicts:
             writer.writerow(dict)
 
-def read_data_from_csv(type):
-    csv_filename = type + '_data.csv'
+        csvfile.close()    
 
-    if type == 'artist':
+def append_data_to_csv(data_dict, type):
+    csv_filename = type + '_data.csv'
+    
+    if type == 'artists':
+        fieldnames = artist_fieldnames
+    
+    with open(csv_filename, 'a', newline='', encoding='UTF8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(data_dict)
+        csvfile.close() 
+
+def load_ids_from_csv(type):
+    csv_filename = type + '_data.csv'
+    id_set = set()
+
+    if type == 'artists':
         fieldnames = artist_fieldnames
 
     with open(csv_filename, newline='', encoding='UTF8') as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
-            print(row[fieldnames[1]])
+            id_set.add(row[fieldnames[0]])
+
+    return id_set        
 
 def read_universe_data_from_csv(type):
     if type == 'artists':
@@ -45,7 +63,10 @@ def read_universe_data_from_csv(type):
         reader = csv.reader(csvfile, delimiter=',')
         all_artist_ids = list(reader)[0]
         return all_artist_ids
-    
+
+def generate_candidate_list(playlist):
+    return playlist
+
 def refresh_access_token():
     print('Refreshing Access Token')
     data = {
@@ -64,106 +85,95 @@ def refresh_access_token():
     print(f'New Token: {access_token}')
     return token_type, access_token
 
-def fetch_artist_data():
-    token_type, access_token = refresh_access_token()
-    auth_payload = token_type + '  ' + access_token
-
-    artist_data_dicts = []
-    all_artist_ids = read_universe_data_from_csv('artists')
-
-    for i in range(0, 250, 50):
-        artist_ids = ','.join(all_artist_ids[i:i+50])
-        print(i)
+def retry_http_call(response, request_url, headers):
+    print(request_url)
+    if response.status_code == 401:
+        token_type, access_token = refresh_access_token()
+        auth_payload = token_type + '  ' + access_token
         headers = {
             'Authorization': auth_payload,
         }
+        response = requests.get(request_url, headers=headers)
+    elif response.status_code == 429:
+        retry_after_secs = int(response.headers['retry-after']) + 1
+        print('Rate Limit Exceeded, sleeping for ' + str(retry_after_secs) + ' seconds')
+        time.sleep(retry_after_secs)
+        response = requests.get(request_url, headers=headers)
 
-        # response = requests.get('https://api.spotify.com/v1/artists/' + artist_id, headers=headers)
-        response_batch = requests.get('https://api.spotify.com/v1/artists?ids=' + artist_ids, headers=headers)
+    return response, headers    
 
-        while response_batch.status_code != 200:
-            if response_batch.status_code == 401:
-                token_type, access_token = refresh_access_token()
-                auth_payload = token_type + '  ' + access_token
-                headers = {
-                    'Authorization': auth_payload,
-                }
-                response_batch = requests.get('https://api.spotify.com/v1/artists?ids=' + artist_ids, headers=headers)
-            elif response_batch.status_code == 429:
-                print('Rate Limit Exceeded, sleeping for 4 seconds')
-                time.sleep(4)
-                response_batch = requests.get('https://api.spotify.com/v1/artists?ids=' + artist_ids, headers=headers)
+def fetch_artist_data():
+    # Get access token and build auth headers
+    token_type, access_token = refresh_access_token()
+    # token_type, access_token = refresh_access_token()
+    auth_payload = token_type + '  ' + access_token
+    headers = {
+        'Authorization': auth_payload,
+    }
+
+    # Load already fetched ids
+    fetched_artists = load_ids_from_csv('artists')
+    
+    # Load all artist ids in the universe
+    all_artist_ids = read_universe_data_from_csv('artists')
+
+    for idx in range(len(all_artist_ids)):
+        artist_id = all_artist_ids[idx]
+
+        # Don't duplicate effort
+        if artist_id in fetched_artists:
+            continue
+
+        # Rate limit throttling
+        # time.sleep(1)
         
-        response_batch_dict = json.loads(response_batch.text)
+        # Populate artist data
+        artist_data = {}
         
-        for artist in response_batch_dict['artists']:
-            artist_data = {}
-            artist_data['artist_id'] = artist['id']
-            artist_data['artist_name'] = artist['name']
-            artist_data['artist_genres'] = artist['genres']
-            artist_data['artist_followers'] = artist['followers']['total']
-            artist_data['artist_popularity'] = artist['popularity']
-            artist_data['artist_type'] = artist['type']
+        response = requests.get('https://api.spotify.com/v1/artists/' + artist_id, headers=headers)
+        
+        while response.status_code != 200:
+            request_url = 'https://api.spotify.com/v1/artists/' + artist_id
+            response, headers = retry_http_call(response, request_url, headers)
+        
+        response_dict = json.loads(response.text)
 
-            # fetch artist top tracks
-            headers = {
-                'Authorization': auth_payload,
-            }
-            response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/top-tracks?market=ES', headers=headers)
+        artist_data['artist_id'] = artist_id
+        artist_data['artist_name'] = response_dict['name']
+        artist_data['artist_genres'] = response_dict['genres']
+        artist_data['artist_followers'] = response_dict['followers']['total']
+        artist_data['artist_popularity'] = response_dict['popularity']
+        artist_data['artist_type'] = response_dict['type']
+
+        # Fetch artist top tracks            
+        artist_data['artist_top_tracks'] = []
+
+        response = requests.get('https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks?market=ES', headers=headers)
+        
+        while response.status_code != 200:
+            request_url = 'https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks?market=ES'
+            response, headers = retry_http_call(response, request_url, headers)
+        
+        response_dict = json.loads(response.text)
+        for track in response_dict['tracks']:
+            artist_data['artist_top_tracks'].append(track['id'])
+        
+        # Fetch related artists
+        artist_data['related_artists'] = []
+        
+        response = requests.get('https://api.spotify.com/v1/artists/' + artist_id + '/related-artists', headers=headers)
+        while response.status_code != 200:
+            request_url = 'https://api.spotify.com/v1/artists/' + artist_id + '/related-artists'
+            response, headers = retry_http_call(response, request_url, headers)
             
-            while response.status_code != 200:
-                if response.status_code == 401:
-                    token_type, access_token = refresh_access_token()
-                    auth_payload = token_type + '  ' + access_token
-                    headers = {
-                        'Authorization': auth_payload,
-                    }
-                    response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/top-tracks?market=ES', headers=headers)
-                elif response.status_code == 429:
-                    print('Rate Limit Exceeded, sleeping for 4 seconds')
-                    time.sleep(4)
-                    response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/top-tracks?market=ES', headers=headers)
-            
-            artist_data['artist_top_tracks'] = []
-            try:
-                response_dict = json.loads(response.text)
-                for track in response_dict['tracks']:
-                    artist_data['artist_top_tracks'].append(track['id'])
-            except Exception as e: # work on python 3.x
-                print('Failed to get top tracks for artist id ' + artist['id'] + ', received response ' + response.text)
-                print('Error Message: ' + str(e))     
+        response_dict = json.loads(response.text)
 
-            # fetch related artists
-            headers = {
-                'Authorization': auth_payload,
-            }
-            response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/related-artists', headers=headers)
+        for rel_artist in response_dict['artists']:
+            artist_data['related_artists'].append(rel_artist['id'])
 
-            while response.status_code != 200:
-                if response.status_code == 401:
-                    token_type, access_token = refresh_access_token()
-                    auth_payload = token_type + '  ' + access_token
-                    headers = {
-                        'Authorization': auth_payload,
-                    }
-                    response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/related-artists', headers=headers)
-                elif response.status_code == 429:
-                    print('Rate Limit Exceeded, sleeping for 4 seconds')
-                    time.sleep(4)
-                    response = requests.get('https://api.spotify.com/v1/artists/' + artist['id'] + '/related-artists', headers=headers)
-            
-            artist_data['related_artists'] = []
-            try:
-                response_dict = json.loads(response.text)
-                for rel_artist in response_dict['artists']:
-                    artist_data['related_artists'].append(rel_artist['id'])
-            except Exception as e: # work on python 3.x
-                print('Failed to get related artists for artist id ' + artist['id'] + ', received response ' + response.text)
-                print('Error Message: ' + str(e))    
-                
-            artist_data_dicts.append(artist_data)
-
-    write_data_to_csv(artist_data_dicts, 'artist')
+        # Append data to csv
+        print('Writing data for artist ' + str(idx) + ': ' + artist_data['artist_name'])
+        append_data_to_csv(artist_data, 'artists')
 
 def fetch_track_data():
     token_type, access_token = refresh_access_token()
